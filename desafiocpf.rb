0abfe3a1db7620bd6980fe06f21af2ef3e042e4a
Gabriel G. Santos <92769189+gabs1996@users.noreply.github.com>
2023-10-04 23:55:34 -0300

    forkdesafiocpf.rb
    Foi solicitado que utilizasse uma biblioteca para validar o número de CPF.

--- desafiocpf.rb
+++ desafiocpf.rb
@@ -1,16 +1,23 @@
+=begin criar um programa de consulta ao cpf do usuário.
+    Seu código precisa utilizar uma biblioteca para saber
+    se os números que o usuário digitou são de um CPF verdadeiro.
+=end
+
 require "cpf_cnpj"
- 
-def check_cpf(cpf_number)
- if CPF.valid?(cpf_number)
-   return "O cpf informado é valido"
- else
-   return "O cpf informado é invalido"
- end
+
+
+
+def check_cpf(numero_cpf)
+    if CPF.valid?(numero_cpf)
+        print "CPF #{numero_cpf} válido."
+    else
+        print "CPF #{numero_cpf} inválido."
+    end
+
 end
- 
-print 'Digite seu cpf: '
-cpf_number = gets.chomp.to_i
- 
-result = check_cpf(cpf_number)
- 
+
+puts "Digite o seu CPF:    "
+numero_cpf = gets.chomp.to_i 
+result = check_cpf(numero_cpf)
+
 puts result
